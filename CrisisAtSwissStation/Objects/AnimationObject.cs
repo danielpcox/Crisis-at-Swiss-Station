b0f4d457590030efb501eda59eeb2c9a9ae34da4
Alexander Cross <atc54@cornell.edu>
2011-05-01 15:12:06 -0400

    made the light infront of the dude, and he can walk through it like a boss

--- CrisisAtSwissStation/Objects/AnimationObject.cs
+++ CrisisAtSwissStation/Objects/AnimationObject.cs
@@ -26,6 +26,7 @@ namespace CrisisAtSwissStation
         [NonSerialized]
         private Texture2D animTexture;
         private int myGameTime, animateTimer, animateInterval;
+        private bool hasBody;
 
         string animTextureName;
 
@@ -46,6 +47,31 @@ namespace CrisisAtSwissStation
             spriteHeight = sprHeight;
             sourceRect = new Rectangle(xFrame * spriteWidth, yFrame * spriteHeight, spriteWidth, spriteHeight);
             origin = new Vector2(sourceRect.Width / 2, sourceRect.Height / 2);
+
+            hasBody = true;
+
+
+        }
+        public AnimationObject(World world, string animTexturename, string objectTexturename, int sprWidth, int sprHeight, int animInt, int myNumFrames, bool hasBody)
+            : base(world, objectTexturename, 0f, .5f, 0.0f, 1, false)
+        {
+            animTextureName = animTexturename;
+            //Console.WriteLine("***" + animTexturename + "***");
+            animTexture = GameEngine.TextureList[animTexturename];
+
+            myGameTime = 0;
+            animateTimer = 0;
+            animateInterval = animInt;
+            xFrame = 0;
+            yFrame = 0;
+            numFrames = myNumFrames;
+            spriteWidth = sprWidth;
+            spriteHeight = sprHeight;
+            sourceRect = new Rectangle(xFrame * spriteWidth, yFrame * spriteHeight, spriteWidth, spriteHeight);
+            origin = new Vector2(sourceRect.Width / 2, sourceRect.Height / 2);
+
+            this.hasBody = hasBody;
+         
 
 
         }
@@ -77,6 +103,8 @@ namespace CrisisAtSwissStation
                 animateTimer = 0;
 
             }
+
+            if (!hasBody) { RemoveFromWorld(); hasBody = true; }
 
             base.Update(world, dt);
 
